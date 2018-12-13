7668fab3501d7c4a6c98f41f90e40acf8d614167
Your Name <you@example.com>
2018-12-13 16:18:00 +0400

    das4i tnayin

---
+++ script.js
@@ -0,0 +1,27 @@
+var clickCount = 0;
+function clickHandler(evt){
+    clickCount++;
+    console.log(evt);
+    var str = "Thanks for clicking " + clickCount;
+    this.innerText = str;
+}
+
+var p = document.getElementById("pElement");
+p.addEventListener("click", clickHandler);
+
+function bodyClick(evt){
+    console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+}
+window.onclick = bodyClick;
+
+function keydown(evt) {
+    console.log("You printed " + evt.key);
+ }
+ window.onkeydown = keydown;
+ 
+ function windowLoad() {
+    console.log("Window is loaded");
+ }
+ window.onload = windowLoad;
+ 
+ 
